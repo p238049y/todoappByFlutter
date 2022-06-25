45a5d1b78f719eec746283fa29313d50ead3c832
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-06-25 22:34:54 +0900

    [update] #1  todoリストの追加ボタンを作成
    [タスク] アプリの雛形を作成す

--- mytodoapp/lib/TodoList/TodoListPage.dart
+++ mytodoapp/lib/TodoList/TodoListPage.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mytodoapp/TodoList/TodoAddPage.dart';
 
 // リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
