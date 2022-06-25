e72d4a08a5821be245918719db456f4738d47620
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-06-25 22:14:42 +0900

    [add] #1 todoアプリのリストページをホーム画面にする
    [タスク] アプリの雛形を作成す

--- mytodoapp/lib/main.dart
+++ mytodoapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'TodoList/TodoListPage.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,10 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: TodoListPage(),
     );
   }
 }
