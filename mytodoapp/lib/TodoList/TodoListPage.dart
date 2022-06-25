a525f00bb2c39de1c626672a7d714926a346db61
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-06-25 22:14:10 +0900

    [add] #1 todoアプリのリストページを追加
    [タスク] アプリの雛形を作成す

---
+++ mytodoapp/lib/TodoList/TodoListPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// リスト一覧画面用Widget
+class TodoListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('リスト一覧画面'),
+      ),
+    );
+  }
+}
