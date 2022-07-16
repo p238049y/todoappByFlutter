676e8beb555c874e0b9099b410387c16a329c65e
p238049y <p238049y@gmail.com>
2022-07-16 16:04:39 +0900

    [add] #6 ストップウォッチ画面で使用するモジュールを作成
    [タスク] ストップウォッチ機能を持った画面を作成

---
+++ mytodoapp/lib/module/StopWatch/stopWatchModel.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class StopWatchModel extends ChangeNotifier {
+  bool isStopPressed = true;
+  bool isResetPressed = true;
+  bool isStartPressed = true;
+
+  String stopWatchTimeDisplay = '00:00:00';
+  var stopWatch = Stopwatch();
+  final duration = const Duration(seconds: 1);
+
+  startTimer () {
+    Timer(duration, keepRunning);
+  }
+
+  keepRunning () {
+    if (stopWatch.isRunning) {
+      startTimer();
+    }
+
+    stopWatchTimeDisplay = '${stopWatch.elapsed.inHours.toString().padLeft(2, '0')}:${(stopWatch.elapsed.inMinutes%60).toString().padLeft(2,"0")}:${(stopWatch.elapsed.inSeconds%60).toString().padLeft(2,"0")}';
+
+    notifyListeners();
+  }
+
+  startStopWatch() {
+    isStopPressed = false;
+    isStartPressed = true;
+    stopWatch.start();
+    startTimer();
+    notifyListeners();
+  }
+
+  resetStopWatch() {
+    isResetPressed = true;
+    isStartPressed = true;
+    stopWatch.reset();
+    stopWatchTimeDisplay = '00:00:00';
+    notifyListeners();
+  }
+}
