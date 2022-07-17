af493f58fdc470eadbe1cc64c001641822a97977
p238049y <p238049y@gmail.com>
2022-07-17 20:07:25 +0900

    [add] #6 ストップウォッチ画面を追加
    [タスク] ストップウォッチ機能を持った画面を作成

--- mytodoapp/lib/Views/StopWatch/StopWatchPage.dart
+++ mytodoapp/lib/Views/StopWatch/StopWatchPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mytodoapp/module/StopWatch/stopWatchModel.dart';
+import 'package:provider/provider.dart';
 
 // class StopWatchPage extends StatelessWidget {
 //   @override
@@ -15,6 +16,55 @@ class StopWatchPage extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.all(30.0),
+        child: ChangeNotifierProvider<StopWatchModel>(
+          create: (_) => StopWatchModel(),
+          child: Consumer<StopWatchModel>(
+            builder: (context, model, child) {
+              return Column(
+                children: [
+                  const SizedBox(height: 100.0),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      model.stopWatchTimeDisplay,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30.0),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50, right: 8),
+                        child: RaisedButton(
+                          color: Colors.redAccent,
+                          onPressed: model.isStopPressed ? null : model.stopStopWatch(),
+                          child: const Text('STOP'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30, right: 8),
+                        child: RaisedButton(
+                          color: Colors.green,
+                          onPressed: model.isResetPressed ? null : model.resetStopWatch(),
+                          child: const Text('RESET'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20.0),
+                  RaisedButton(
+                    color: Colors.orange,
+                    onPressed: model.isStartPressed ? model.startStopWatch : null,
+                    child: const Text('START'),
+                  ),
+                ],
+              );
+            }
+          )
+        ),
       ),
     );
   }
