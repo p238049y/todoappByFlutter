a10447e4b0926182ee30c6ae416435cfbbd728ba
p238049y <p238049y@gmail.com>
2022-07-20 23:04:12 +0900

    [add] #9 タイマー画面の雛形を作成
    [タスク] タイマー画面を作成する

---
+++ mytodoapp/lib/Views/StopWatch/TimerPage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:mytodoapp/module/StopWatch/stopWatchModel.dart';
+import 'package:provider/provider.dart';
+
+class TimerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: ChangeNotifierProvider<StopWatchModel>(
+          create: (_) => StopWatchModel(),  
+          child: Consumer<StopWatchModel>(
+            builder: (context, model, child) {
+              return Column(
+                children: [
+                  const SizedBox(height: 100.0),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      model.stopWatchTimeDisplay,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30.0),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 70, right: 8),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.red,
+                            onPrimary: Colors.grey,
+                          ),
+                          onPressed: model.isStopPressed ? null : model.stopStopWatch,
+                          child: const Text('STOP',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30, right: 8),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                            onPrimary: Colors.grey,
+                          ),
+                          onPressed: model.isResetPressed ? null : model.resetStopWatch,
+                          child: const Text('RESET',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      onPrimary: Colors.grey,
+                    ),
+                    onPressed: model.isStartPressed ? model.startStopWatch : null,
+                    child: const Text('START',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              );
+            }
+          )
+        ),
+      ),
+    );
+  }
+}
---
+++ mytodoapp/lib/module/StopWatch/timerModel.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimerModel extends ChangeNotifier {
+  bool isStopPressed = true;
+  bool isResetPressed = true;
+  bool isStartPressed = true;
+
+  String timerDisplay = '99:99:99';
+  var stopWatch = Stopwatch();
+  final duration = const Duration(seconds: 1);
+
+  startTimer () {
+    Timer(duration, keepRunning);
+  }
+
+  keepRunning () {
+    if (stopWatch.isRunning) {
+      startTimer();
+    }
+
+    timerDisplay = '${stopWatch.elapsed.inHours.toString().padLeft(2, '0')}:${(stopWatch.elapsed.inMinutes%60).toString().padLeft(2,"0")}:${(stopWatch.elapsed.inSeconds%60).toString().padLeft(2,"0")}';
+
+    notifyListeners();
+  }
+
+  startStopWatch() {
+    isStopPressed = false;
+    isStartPressed = true;
+    stopWatch.start();
+    startTimer();
+    notifyListeners();
+  }
+
+  stopStopWatch() {
+    isStopPressed = true;
+    isResetPressed = false;
+    stopWatch.stop();
+    notifyListeners();
+  }
+
+  resetStopWatch() {
+    isResetPressed = true;
+    isStartPressed = true;
+    stopWatch.reset();
+    timerDisplay = '99:99:99';
+    notifyListeners();
+  }
+}
