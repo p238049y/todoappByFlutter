23fa7d75e40c3d5c24694aa721dd1f66d39e1737
p238049y <p238049y@gmail.com>
2022-07-24 16:47:05 +0900

    [update] #9 スタートボタン、ストップボタンをクリックした時に発火するメソッドを追加
    [タスク] タイマー画面を作成する

--- mytodoapp/lib/module/StopWatch/timerModel.dart
+++ mytodoapp/lib/module/StopWatch/timerModel.dart
@@ -8,47 +8,53 @@ class TimerModel extends ChangeNotifier {
   int sec = 0;
 
   bool isStopPressed = true;
-  bool isResetPressed = true;
   bool isStartPressed = true;
 
-  String timerDisplay = '99:99:99';
-  var stopWatch = Stopwatch();
-  final duration = const Duration(seconds: 1);
+
+  int timerDisplay = 0;
+  // var stopWatch = Stopwatch();
+  // final duration = const Duration(seconds: 1);
 
   startTimer () {
-    Timer(duration, keepRunning);
+    // timerDisplay = ()
+    // Timer(duration, keepRunning);
   }
 
-  keepRunning () {
-    if (stopWatch.isRunning) {
-      startTimer();
-    }
-
-    timerDisplay = '${stopWatch.elapsed.inHours.toString().padLeft(2, '0')}:${(stopWatch.elapsed.inMinutes%60).toString().padLeft(2,"0")}:${(stopWatch.elapsed.inSeconds%60).toString().padLeft(2,"0")}';
-
-    notifyListeners();
+  stopTimer () {
+    // timerDisplay = ()
+    // Timer(duration, keepRunning);
   }
 
-  startStopWatch() {
-    isStopPressed = false;
-    isStartPressed = true;
-    stopWatch.start();
-    startTimer();
-    notifyListeners();
-  }
+  // keepRunning () {
+  //   if (stopWatch.isRunning) {
+  //     startTimer();
+  //   }
 
-  stopStopWatch() {
-    isStopPressed = true;
-    isResetPressed = false;
-    stopWatch.stop();
-    notifyListeners();
-  }
+  //   timerDisplay = '${stopWatch.elapsed.inHours.toString().padLeft(2, '0')}:${(stopWatch.elapsed.inMinutes%60).toString().padLeft(2,"0")}:${(stopWatch.elapsed.inSeconds%60).toString().padLeft(2,"0")}';
 
-  resetStopWatch() {
-    isResetPressed = true;
-    isStartPressed = true;
-    stopWatch.reset();
-    timerDisplay = '99:99:99';
-    notifyListeners();
-  }
+  //   notifyListeners();
+  // }
+
+  // startStopWatch() {
+  //   isStopPressed = false;
+  //   isStartPressed = true;
+  //   stopWatch.start();
+  //   startTimer();
+  //   notifyListeners();
+  // }
+
+  // stopStopWatch() {
+  //   isStopPressed = true;
+  //   // isResetPressed = false;
+  //   stopWatch.stop();
+  //   notifyListeners();
+  // }
+
+  // resetStopWatch() {
+  //   // isResetPressed = true;
+  //   isStartPressed = true;
+  //   stopWatch.reset();
+  //   timerDisplay = '99:99:99';
+  //   notifyListeners();
+  // }
 }
