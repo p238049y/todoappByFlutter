aeee98caeae40e099affa050c9bed39fed2b2565
p238049y <p238049y@gmail.com>
2022-08-02 23:13:39 +0900

    [update] #12 タスクの情報詳細画面の雛形を修正
    [タスク] タスクの情報詳細画面の作成

--- mytodoapp/lib/Views/TodoList/TodoDetailPage.dart
+++ mytodoapp/lib/Views/TodoList/TodoDetailPage.dart
@@ -8,7 +8,6 @@ class TodoDetailPage extends StatefulWidget {
 }
 
 class _TodoDetailPage extends State<TodoDetailPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,41 +18,106 @@ class _TodoDetailPage extends State<TodoDetailPage> {
         padding: const EdgeInsets.all(50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: const <Widget>[
-            Text(
-              'やること',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 24),
-            Text(
-              '実施日',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 24),
-            Text(
-              '詳細内容',
-              style: TextStyle(
-                fontSize: 20,
-              ),              
-            ),
-            SizedBox(height: 24),
-            Text(
-              '目標時間',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 24),
-            Text(
-              '経過時間',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
+          children: <Widget>[
+            Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text(
+                            'やること',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        Expanded(
+                           flex: 4,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text('やること', style: TextStyle(fontSize: 18.0),),
+                          )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text(
+                            '実施日',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        Expanded(
+                           flex: 4,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text('実施日', style: TextStyle(fontSize: 18.0),),
+                          )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text(
+                            '詳細内容',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        Expanded(
+                           flex: 4,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text('詳細内容', style: TextStyle(fontSize: 18.0),),
+                          )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text(
+                            '目標時間',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        Expanded(
+                           flex: 4,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text('目標時間', style: TextStyle(fontSize: 18.0),),
+                          )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Expanded(
+                          flex: 2,
+                          child: Text(
+                            '経過時間',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        Expanded(
+                           flex: 4,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text('経過時間', style: TextStyle(fontSize: 18.0),),
+                          )
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
