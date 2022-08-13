0c3fcf115221171c18a85dec4e2614b301271cba
p238049y <p238049y@gmail.com>
2022-08-13 17:39:06 +0900

    [clean]#17 フォーマットをかける
    [タスク] 時間管理画面の上部にTODOタイトルを表示する

--- mytodoapp/lib/Views/StopWatch/TimerPage.dart
+++ mytodoapp/lib/Views/StopWatch/TimerPage.dart
@@ -19,123 +19,124 @@ class _TimerPage extends State<TimerPage> with TickerProviderStateMixin {
         create: (_) => TimerModel(),
         child: Consumer<TimerModel>(builder: (context, model, child) {
           return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  flex: 6,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 10.0,
-                              ),
-                              child: Text(
-                                "Hour",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            NumberPicker(
-                              minValue: 0,
-                              maxValue: 23,
-                              value: model.hour,
-                              onChanged: (val) => model.changeHourVal(val),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 10.0,
-                              ),
-                              child: Text(
-                                "Min",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            NumberPicker(
-                              minValue: 0,
-                              maxValue: 59,
-                              value: model.min,
-                              onChanged: (val) => model.changeMinuteVal(val),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 10.0,
-                              ),
-                              child: Text(
-                                "Sec",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            NumberPicker(
-                              minValue: 0,
-                              maxValue: 59,
-                              value: model.sec,
-                              onChanged: (val) => model.changeSecondVal(val),
-                            ),
-                          ],
-                        ),
-                      ]),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    model.timeToDisplay,
-                    style: const TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(displayData.text),
+              Expanded(
+                flex: 6,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.orange,
-                          onPrimary: Colors.grey,
-                        ),
-                        onPressed:
-                            model.isStartPressed ? model.startTimer : null,
-                        child: const Text(
-                          'START',
-                          style: TextStyle(color: Colors.white, fontSize: 18.0),
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.only(
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              "Hour",
+                              style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          NumberPicker(
+                            minValue: 0,
+                            maxValue: 23,
+                            value: model.hour,
+                            onChanged: (val) => model.changeHourVal(val),
+                          ),
+                        ],
                       ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.green,
-                          onPrimary: Colors.grey,
-                        ),
-                        onPressed: model.isStopPressed ? model.stopTimer : null,
-                        child: const Text(
-                          'STOP',
-                          style: TextStyle(color: Colors.white, fontSize: 18.0),
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.only(
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              "Min",
+                              style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          NumberPicker(
+                            minValue: 0,
+                            maxValue: 59,
+                            value: model.min,
+                            onChanged: (val) => model.changeMinuteVal(val),
+                          ),
+                        ],
                       ),
-                    ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.only(
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              "Sec",
+                              style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          NumberPicker(
+                            minValue: 0,
+                            maxValue: 59,
+                            value: model.sec,
+                            onChanged: (val) => model.changeSecondVal(val),
+                          ),
+                        ],
+                      ),
+                    ]),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  model.timeToDisplay,
+                  style: const TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w600,
                   ),
-                )
-              ]);
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                        onPrimary: Colors.grey,
+                      ),
+                      onPressed:
+                          model.isStartPressed ? model.startTimer : null,
+                      child: const Text(
+                        'START',
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        onPrimary: Colors.grey,
+                      ),
+                      onPressed: model.isStopPressed ? model.stopTimer : null,
+                      child: const Text(
+                        'STOP',
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ]);
         }),
       ),
     ));
