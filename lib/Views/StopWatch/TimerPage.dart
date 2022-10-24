677a9993a8188cf0aadbd45e0277d222ed2d59c4
p238049y <p238049y@gmail.com>
2022-10-24 23:11:39 +0900

    [fix]#32 コードフォーマットをかける
    [タスク] タイマー画面で時間経過を表す部分の周りにCircularProgressIndicatorを導入する

--- lib/Views/StopWatch/TimerPage.dart
+++ lib/Views/StopWatch/TimerPage.dart
@@ -32,152 +32,144 @@ class _TimerPage extends State<TimerPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: ChangeNotifierProvider<TimerModel>(
-        create: (_) => TimerModel(),
-        child: Consumer<TimerModel>(builder: (context, model, child) {
-          return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 50.0),
-                Text(
-                  displayData.text,
-                  style: const TextStyle(
-                      fontSize: 36.0, fontWeight: FontWeight.bold),
-                ),
-                Expanded(
-                  flex: 6,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 10.0,
-                              ),
-                              child: Text(
-                                "Hour",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            NumberPicker(
-                              minValue: 0,
-                              maxValue: 23,
-                              value: model.hour,
-                              onChanged: (val) => model.changeHourVal(val),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 10.0,
-                              ),
-                              child: Text(
-                                "Min",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            NumberPicker(
-                              minValue: 0,
-                              maxValue: 59,
-                              value: model.min,
-                              onChanged: (val) => model.changeMinuteVal(val),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 10.0,
-                              ),
-                              child: Text(
-                                "Sec",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            NumberPicker(
-                              minValue: 0,
-                              maxValue: 59,
-                              value: model.sec,
-                              onChanged: (val) => model.changeSecondVal(val),
-                            ),
-                          ],
-                        ),
-                      ]),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    model.timeToDisplay,
-                    style: const TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: ChangeNotifierProvider<TimerModel>(
+      create: (_) => TimerModel(),
+      child: Consumer<TimerModel>(builder: (context, model, child) {
+        return Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          const SizedBox(height: 50.0),
+          Text(
+            displayData.text,
+            style: const TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            flex: 6,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.orange,
-                          onPrimary: Colors.grey,
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 10.0,
                         ),
-                        onPressed:
-                            model.isStartPressed ? model.startTimer : null,
-                        child: const Text(
-                          'START',
-                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        child: Text(
+                          "Hour",
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
                         ),
                       ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.green,
-                          onPrimary: Colors.grey,
-                        ),
-                        onPressed: model.isStopPressed ? model.stopTimer : null,
-                        child: const Text(
-                          'STOP',
-                          style: TextStyle(color: Colors.white, fontSize: 18.0),
-                        ),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.blue,
-                          onPrimary: Colors.grey,
-                        ),
-                        onPressed: () async {
-                          displayData.targetTime = model.initialSettingTimeToDisplay;
-                          displayData.elapsedTime = model.elapsedTimeToDisplay;
-                          await DbProvider.insertData(displayData);
-                          Navigator.of(context).pop(displayData);
-                        },
-                        child: const Text(
-                          'ADD',
-                          style: TextStyle(color: Colors.white, fontSize: 18.0),
-                        ),
+                      NumberPicker(
+                        minValue: 0,
+                        maxValue: 23,
+                        value: model.hour,
+                        onChanged: (val) => model.changeHourVal(val),
                       ),
                     ],
                   ),
-                )
-              ]);
-        }),
-      ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 10.0,
+                        ),
+                        child: Text(
+                          "Min",
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      NumberPicker(
+                        minValue: 0,
+                        maxValue: 59,
+                        value: model.min,
+                        onChanged: (val) => model.changeMinuteVal(val),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 10.0,
+                        ),
+                        child: Text(
+                          "Sec",
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      NumberPicker(
+                        minValue: 0,
+                        maxValue: 59,
+                        value: model.sec,
+                        onChanged: (val) => model.changeSecondVal(val),
+                      ),
+                    ],
+                  ),
+                ]),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(
+              model.timeToDisplay,
+              style: const TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    onPrimary: Colors.grey,
+                  ),
+                  onPressed: model.isStartPressed ? model.startTimer : null,
+                  child: const Text(
+                    'START',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    onPrimary: Colors.grey,
+                  ),
+                  onPressed: model.isStopPressed ? model.stopTimer : null,
+                  child: const Text(
+                    'STOP',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.grey,
+                  ),
+                  onPressed: () async {
+                    displayData.targetTime = model.initialSettingTimeToDisplay;
+                    displayData.elapsedTime = model.elapsedTimeToDisplay;
+                    await DbProvider.insertData(displayData);
+                    Navigator.of(context).pop(displayData);
+                  },
+                  child: const Text(
+                    'ADD',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]);
+      }),
     ));
   }
 }
